342c173b4526d66b27be9445dd7bebe6a8e06ce8
Nhat-Minh Ta <minh772k2@gmail.com>
2024-09-27 16:27:38 +0700

    upload

--- frontend/quizA1.js
+++ frontend/quizA1.js
@@ -4,7 +4,7 @@ let userAnswers = {}; // Lưu trữ lựa chọn đáp án của người dùng
 
 // Hàm gọi API để lấy danh sách câu hỏi từ server
 function loadQuestions() {
-  fetch("http://localhost:3000/A1_API")
+  fetch("http://sql306.infinityfree.com:3306/A1_API")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
